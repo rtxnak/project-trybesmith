881ab4ca1e3037cd0d3e7a1631ac79469b8047f5
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-05-13 19:04:36 -0300

    adicionou model para Endpoint POST no /products

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -1,3 +1,4 @@
+import { ResultSetHeader } from 'mysql2/promise';
 import connection from './connection';
 import IProduct from '../interfaces/products.interface';
 
@@ -7,5 +8,13 @@ export default class ProductModel {
       'SELECT * FROM Trybesmith.Products',
     );
     return product as IProduct[];
+  };
+
+  public create = async (name: string, amount: string): Promise<IProduct> => {
+    const [product] = await connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?);',
+      [name, amount],
+    );
+    return { id: product.insertId, name, amount };
   };
 }
