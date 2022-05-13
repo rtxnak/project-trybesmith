1597ca3421dee3f4a1667a25e796544f08c6b766
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-05-13 19:08:27 -0300

    adicionou route e middleware de validacao para Endpoint POST no /products

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import ProductController from '../controllers/productController';
+import productMiddleware from '../middlewares/productMiddleware';
 
 const productController = new ProductController();
 
 const route = Router();
 
 route.get('/', productController.getAll);
+
+route.use(productMiddleware);
+
+route.post('/', productController.create);
 
 export default route;
