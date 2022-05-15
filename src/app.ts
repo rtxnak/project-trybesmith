c6e875c2ad2b5978c5f1489a124e1b50965671d0
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-05-15 00:13:31 -0300

    feat: adicionou o path /users

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import productRoutes from './routes/productRoutes';
+import userRoutes from './routes/userRoutes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/products', productRoutes);
+app.use('/users', userRoutes);
 
 export default app;
