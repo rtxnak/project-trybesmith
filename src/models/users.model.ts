050d08d0494a3b33cfcf95a855b6455b570ae4af
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-05-16 18:48:55 -0300

    adicionou model para Endpoint POST no /login

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -1,12 +1,13 @@
 import { ResultSetHeader } from 'mysql2/promise';
 import connection from './connection';
 import IUser from '../interfaces/users.interface';
+import ILogin from '../interfaces/login.interface';
 
 export default class UsersModel {
   public create = async (
-    username: string, 
-    classe: string, 
-    level: number, 
+    username: string,
+    classe: string,
+    level: number,
     password: string,
   ): Promise<IUser> => {
     const [user] = await connection.execute<ResultSetHeader>(
@@ -14,5 +15,17 @@ export default class UsersModel {
       [username, classe, level, password],
     );
     return { id: user.insertId, username, classe, level, password };
+  };
+
+  public findOne = async (
+    username: string,
+    password: string,
+  ) => {
+    const [row] = await connection.execute(
+      'SELECT * FROM Trybesmith.Users  WHERE username = ? and password = ? ',
+      [username, password],
+    );
+    const user = row as ILogin[];
+    return user[0];
   };
 }
